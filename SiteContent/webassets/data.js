a51f060e5053aacde4e11f858e4edc41065b8a9d
ivouwa <gardnena000@gmail.com>
2026-07-25 04:50:06 -0600

    update js, fix js upgrade on all enemies and claw curses pages

--- SiteContent/webassets/data.js
+++ SiteContent/webassets/data.js
@@ -6,6 +6,54 @@ fetch('pagedetails.json')
     })
 
     .then(data => {
+    let theme = data.theme;
+    let htmlcontainer = document.getElementById('htmlcontainer')
+
+    if (true == true) {
+    htmlcontainer.innerHTML += `
+
+    <body class="${theme}border">
+
+
+        <topbar class="topbar">
+
+                <div class="logo">
+
+                    <a href="../Home/">
+                        <img src="${backtrack}webassets/${theme}logo.png" class="icon">
+                    </a>
+
+                </div>
+
+                <div class="page" id="pagename"></div>
+                
+                <div class="${theme}border">
+
+                        <img src="./icon.png" class="icon"> 
+
+                </div>
+
+        </topbar>
+
+
+        <detail class="detail" id="detail">
+        
+
+        </detail>
+
+
+    </body>
+    `}
+
+    let details = document.getElementById('detail');
+
+    
+
+
+
+        // inserts layout, with themeing rules
+
+
 
         // shows data on urlbar -- errors because this must be in the page
         let urlbar = document.getElementById('urlbar')
@@ -23,122 +71,130 @@ fetch('pagedetails.json')
         pagename.innerHTML=`<a onclick="history.back()" class="page">${data.pagename}`            
         }
 
-        // Titles, can add more if needed
-        let title1 = document.getElementById('title1')
-        if (title1 == undefined) {
-            console.log("No data for title1, skipping")
-        } else{
-        title1.innerHTML=`${data.title1}`            
+
+        
+
+    // Section 1
+   
+        let s1t1 = data.s1t1
+        if (s1t1 == undefined){
+            console.log("No data found for Section 1 Title 1, skipping element.")  
+        }
+        else {
+            details.innerHTML += `<div class="${theme}title">${s1t1}</div>`
         }
 
-        let title2 = document.getElementById('title2')
-        if (title2 == undefined) {
-            console.log("No data for title2, skipping")
-        } else{
-        title2.innerHTML=`${data.title2}`          
+        let s1d1 = data.s1d1
+        if (s1d1 == undefined){
+            console.log("No data found for Section 1 Detail 1, skipping element.")  
+        }
+        else {
+            details.innerHTML += `<div class="subtitle">${s1d1}</div>`
         }
 
-        let title3 = document.getElementById('title3')
-        if (title3 == undefined) {
-            console.log("No data for title3, skipping")
-        } else{
-        title3.innerHTML=`${data.title3}`            
+
+    // Section 2
+
+        let s2t1 = data.s2t1
+        if (s2t1 == undefined){
+            console.log("No data found for Section 2 Title 1, skipping element.")  
+        }
+        else {
+            details.innerHTML += `<div class="${theme}title">${s2t1}</div>`
         }
 
-        let title4 = document.getElementById('title4')
-        if (title4 == undefined) {
-            console.log("No data for title4, skipping")
-        } else{
-        title4.innerHTML=`${data.title4}`            
+
+        let s2d1 = data.s2d1
+        if (s2d1 == undefined){
+            console.log("No data found for Section 2 Detail 1, skipping element.")  
         }
+        else {
+            details.innerHTML += `<div class="subtitle">${s2d1}</div>`
+        }
+        
+        let s2d2 = data.s2d2
+        if (s2d2 == undefined){
+            console.log("No data found for Section 2 Detail 2, skipping element.")  
+        }
+        else {
+            details.innerHTML += `<div class="subtitle">${s2d2}</div>`
+        }
+
+        let s2d3 = data.s2d3
+        if (s2d3 == undefined){
+            console.log("No data found for Section 2 Detail 3, skipping element.")  
+        }
+        else {
+            details.innerHTML += `<div class="subtitle">${s2d3}</div>`
+        }
+        
+        let s2d4 = data.s2d4
+        if (s2d4 == undefined){
+            console.log("No data found for Section 2 Detail 1, skipping element.")  
+        }
+        else {
+            details.innerHTML += `<div class="subtitle">${s2d4}</div>`
+        }
+
+        // section three
+        
+        let s3t1 = data.s3t1
+        if (s3t1 == undefined){
+            console.log("No data found for Section 2 Detail 1, skipping element.")  
+        }
+        else {
+            details.innerHTML += `<div class="${theme}title">${s3t1}</div>`
+        }
+        
+
+        let s3d1 = data.s3d1
+        if (s3d1 == undefined){
+            console.log("No data found for Section 3 Detail 1, skipping element.")  
+        }
+        else {
+            details.innerHTML += `<div class="subtitle">${s3d1}</div>`
+        }
+
+        let s3d2 = data.s3d2
+        if (s3d2 == undefined){
+            console.log("No data found for Section 3 Detail 1, skipping element.")  
+        }
+        else {
+            details.innerHTML += `<div class="subtitle">${s3d2}</div>`
+        }
+
+        // section three
+        
+        let s4t1 = data.s4t1
+        if (s4t1 == undefined){
+            console.log("No data found for Section 2 Detail 1, skipping element.")  
+        }
+        else {
+            details.innerHTML += `<div class="${theme}title">${s4t1}</div>`
+        }
+
+
+
+
+
 
 
         
-        //Details, can add more if needed.
-        let detail1 = document.getElementById('detail1')
-        if (detail1 == undefined) {
-            console.log("No data for detail1, skipping")
-        } else{
-        detail1.innerHTML=`${data.detail1}`            
-        }
-
-        let detail2 = document.getElementById('detail2')
-        if (detail2 == undefined) {
-            console.log("No data for detail2, skipping")
-        } else{
-        detail2.innerHTML=`${data.detail2}`            
-        }
-
-        let detail3 = document.getElementById('detail3')
-        if (detail3 == undefined) {
-            console.log("No data for detail3, skipping")
-        } else{
-        detail3.innerHTML=`${data.detail3}`            
+        
+        if (extra){
+            htmlcontainer.innerHTML += `${extra} <div></div>`
+        } else {
+            
         }
         
-        let detail4 = document.getElementById('detail4')
-        if (detail4 == undefined) {
-            console.log("No data for detail4, skipping")
-        } else{
-        detail4.innerHTML=`${data.detail4}`            
-        }
-
-        let detail5 = document.getElementById('detail5')
-        if (detail5 == undefined) {
-            console.log("No data for detail5, skipping")
-        } else{
-        detail5.innerHTML=`${data.detail5}`            
-        }
-
-
-        let detail6 = document.getElementById('detail6')
-        if (detail6 == undefined) {
-            console.log("No data for detail6, skipping")
-        } else{
-        detail6.innerHTML=`${data.detail6}`            
-        }
-
-        let detail7 = document.getElementById('detail7')
-        if (detail7 == undefined) {
-            console.log("No data for detail7, skipping")
-        } else{
-        detail7.innerHTML=`${data.detail7}`            
-        }
-
-
-
-
-        // staying a const because this must be in your page.
-        // Keep footer as "----------" in the json! if it's undefined it will error
-        const footer = document.getElementById('footer')
-        if (footer == undefined) {
-            console.error("Possible data error!")
-        }
-        footer.innerHTML = `
+        htmlcontainer.innerHTML += `
+        <footer id="footer">
 
         Made by Ivouwa in 2026 | 
         <a href="https://github.com/Ivouwa/Plantscape-wiki" target="_blank" rel="noopener noreferrer">Want to contribute? View the github</a> | 
         <a href="https://discord.gg/NZrrj6rdRX" target="_blank" rel="noopener noreferrer">Join the plantscape server</a>
         
+        </footer>
         `
     })
     
-    /*
-
-
-    let nameofelement = document.getElementById('nameofelement')
-        if (nameofelement == undefined) {
-            console.log("No data for nameofelement, skipping")
-        } else{
-        nameofelement.innerHTML=`${data.nameofelement}`            
-        }
-
- 
-    
-    ^   if you want to add a new text element, assign the id "nameofelement" to an html element
-        in the json add "nameofelement": "elementdetails",
-
-        replace every "nameofelement" in this example to whatever you choose
-
-
-    */
